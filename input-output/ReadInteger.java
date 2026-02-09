24d687fe62dc422f51b91d7b76f027c9c4717401
Sai Keerthana Buchupalli <keerthana.22mic7127@vitapstudent.ac.in>
2026-02-09 16:55:36 +0530

    add program to read integer

---
+++ input-output/ReadInteger.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class ReadInteger{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    System.out.println(num);
+  }
+}
