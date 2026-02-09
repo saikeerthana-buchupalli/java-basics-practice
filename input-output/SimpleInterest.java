9720082e96acb127e2e09f92cfab7c852a4a81bc
Sai Keerthana Buchupalli <keerthana.22mic7127@vitapstudent.ac.in>
2026-02-09 16:58:44 +0530

    add program to simple interest

---
+++ input-output/SimpleInterest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double p = sc.nextDouble();
+        double r = sc.nextDouble();
+        double t = sc.nextDouble();
+
+        double simpleinterest = (p * r * t) / 100;
+        System.out.println(simpleinterest);
+    }
+}
