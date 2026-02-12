0d1b40c729327d1b2b4e560eedc2bb2467808905
Sai Keerthana Buchupalli <keerthana.22mic7127@vitapstudent.ac.in>
2026-02-12 06:35:49 +0530

    add program to find second largest element in array

---
+++ arrays/SecondLargestElement.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class SecondLargestElement{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(arr[i]>first){
+                second=first;
+                first=arr[i];
+            }else if(arr[i]>second && arr[i] != first){
+                second = arr[i];
+            }
+        }
+        System.out.println(second);
+    }
+}
